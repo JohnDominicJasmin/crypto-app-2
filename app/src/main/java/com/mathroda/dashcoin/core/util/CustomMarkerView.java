f9d5f06e0bc737e2a1fb58ca75374465ed26f414
John_Dominic <johndominicjasmin@gmail.com>
2022-07-17 20:57:15 +0800

    Add gesture callback in chart and add small circle to it.

--- app/src/main/java/com/mathroda/dashcoin/core/util/CustomMarkerView.java
+++ app/src/main/java/com/mathroda/dashcoin/core/util/CustomMarkerView.java
@@ -1,6 +1,7 @@
 package com.mathroda.dashcoin.core.util;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.components.MarkerView;
@@ -10,24 +11,25 @@ import com.github.mikephil.charting.utils.MPPointF;
 import com.mathroda.dashcoin.R;
 
 public class CustomMarkerView extends MarkerView {
-
-    private TextView tvContent;
+    private float yValue = 0;
+    private final View tvContent;
     public CustomMarkerView (Context context, int layoutResource) {
         super(context, layoutResource);
-        // this markerview only displays a textview
-        tvContent = (TextView) findViewById(R.id.tvContent);
+        tvContent = (View) findViewById(R.id.circleIndicator);
     }
 
-    // callbacks everytime the MarkerView is redrawn, can be used to update the
-// content (user-interface)
+
     @Override
     public void refreshContent(Entry e, Highlight highlight) {
-
-        tvContent.setText("$" + e.getY());
-
-        // this will perform necessary layouting
+        yValue = e.getY();
+        tvContent.setVisibility(View.VISIBLE);
         super.refreshContent(e, highlight);
     }
+
+    public float getYEntry(){
+        return yValue;
+    }
+
 
     private MPPointF mOffset;
 
@@ -36,7 +38,7 @@ public class CustomMarkerView extends MarkerView {
 
         if(mOffset == null) {
             // center the marker horizontally and vertically
-            mOffset = new MPPointF(-(getWidth() / 2), -getHeight());
+            mOffset = new MPPointF(-(getWidth() / 2), -(getWidth() / 2));
         }
 
         return mOffset;
--- app/src/main/java/com/mathroda/dashcoin/feature_coins/presentation/coin_detail/components/CoinDetailChart.kt
+++ app/src/main/java/com/mathroda/dashcoin/feature_coins/presentation/coin_detail/components/CoinDetailChart.kt
@@ -1,14 +1,21 @@
 package com.mathroda.dashcoin.feature_coins.presentation.coin_detail.components
 
 import android.content.Context
+import android.view.MotionEvent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.listener.ChartTouchListener
+import com.github.mikephil.charting.listener.OnChartGestureListener
 import com.mathroda.dashcoin.R
 import com.mathroda.dashcoin.core.util.CustomMarkerView
+import com.mathroda.dashcoin.core.util.MyYAxisValueFormatter
 import com.mathroda.dashcoin.feature_coins.domain.models.ChartModel
 import com.mathroda.dashcoin.feature_coins.presentation.coin_detail.utils.ChartScreenViewState
 import com.mathroda.dashcoin.feature_coins.presentation.coin_detail.utils.setLineDataSet
@@ -20,10 +27,11 @@ fun CoinDetailChart(
     modifier: Modifier,
     chartModel: ChartModel?,
     oneDayChange: Double,
-    context: Context
+    context: Context,
+    onChartGesture:(Float) -> Unit
 ) {
 
-    val mv = CustomMarkerView(context, R.layout.marker_view)
+    val markerView = CustomMarkerView(context, R.layout.marker_view)
     val dataSet = mutableListOf<Entry>()
     chartModel?.let { chartsValue ->
         chartsValue.chart.map { value ->
@@ -36,41 +44,83 @@ fun CoinDetailChart(
     AndroidView(
         factory = { contextFactory ->
             LineChart(contextFactory).apply {
+                onChartGestureListener = object: OnChartGestureListener{
+                    override fun onChartGestureStart(
+                        me: MotionEvent?,
+                        lastPerformedGesture: ChartTouchListener.ChartGesture?) {
+
+                        onChartGesture(markerView.yEntry)
+                    }
+
+                    override fun onChartGestureEnd(
+                        me: MotionEvent?,
+                        lastPerformedGesture: ChartTouchListener.ChartGesture?) {
+
+                    }
+
+                    override fun onChartLongPressed(me: MotionEvent?) {
+                    }
+
+                    override fun onChartDoubleTapped(me: MotionEvent?) {
+
+                    }
+
+                    override fun onChartSingleTapped(me: MotionEvent?) {
+
+                    }
+
+                    override fun onChartFling(
+                        me1: MotionEvent?,
+                        me2: MotionEvent?,
+                        velocityX: Float,
+                        velocityY: Float) {
+
+                    }
+
+                    override fun onChartScale(me: MotionEvent?, scaleX: Float, scaleY: Float) {
+
+                    }
+
+                    override fun onChartTranslate(me: MotionEvent?, dX: Float, dY: Float) {
+                    }
+                }
                 description.isEnabled = false
-                isDragEnabled = true
                 xAxis.isEnabled = false
-                axisLeft.setDrawAxisLine(false)
                 axisLeft.textColor = TextWhite.toArgb()
+                axisLeft.valueFormatter = MyYAxisValueFormatter()
                 axisRight.isEnabled = false
+                axisLeft.axisLineColor = Color.Red.toArgb()
+                axisLeft.setDrawAxisLine(false)
+
                 legend.isEnabled = false
-                isDoubleTapToZoomEnabled = true
-                isHighlightPerDragEnabled = true
                 setTouchEnabled(true)
-                setScaleEnabled(true)
+                setScaleEnabled(false)
                 setDrawGridBackground(false)
-                animateX(1)
+                animateX(1, Easing.EaseInOutBounce)
                 setDrawBorders(false)
                 setDrawMarkers(true)
-                marker = mv
+
+                marker = markerView
             }
         },
         update = { lineChart ->
 
-            val lineDataSet =
+             val lineDataSet =
                 ChartScreenViewState().getLineDataSet(
                     lineData = dataSet,
                     label = "chart values",
                     oneDayChange = oneDayChange,
                     context = context,
-                )
+                ).apply {
+                   mode = LineDataSet.Mode.LINEAR
+
+
+                }
 
 
             lineChart.apply {
-
                 setLineDataSet(lineDataSet)
                 setNoDataText("No Data Available")
-
-
                 invalidate()
             }
         },
