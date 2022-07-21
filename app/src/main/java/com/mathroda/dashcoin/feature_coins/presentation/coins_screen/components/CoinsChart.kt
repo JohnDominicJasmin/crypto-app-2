76d336b5cda6f2b4d7ca991af4fdff396a9178c6
John_Dominic <johndominicjasmin@gmail.com>
2022-07-21 19:49:11 +0800

    Change graph line width and display last twelve hours update.

--- app/src/main/java/com/mathroda/dashcoin/feature_coins/presentation/coins_screen/components/CoinsChart.kt
+++ app/src/main/java/com/mathroda/dashcoin/feature_coins/presentation/coins_screen/components/CoinsChart.kt
@@ -1,17 +1,14 @@
 package com.mathroda.dashcoin.feature_coins.presentation.coins_screen.components
 
 import android.content.Context
-import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.mathroda.dashcoin.R
-import com.mathroda.dashcoin.core.util.Constants.LAST_FIVE_HOURS
+import com.mathroda.dashcoin.core.util.Constants.LAST_TWELVE_HOURS
 import com.mathroda.dashcoin.feature_coins.domain.models.ChartModel
 import com.mathroda.dashcoin.feature_coins.presentation.coin_detail.components.addEntry
 import com.mathroda.dashcoin.feature_coins.presentation.coin_detail.utils.ChartScreenViewState
@@ -30,7 +27,7 @@ fun CoinsChart(
 
     val dataSet = mutableListOf<Entry>()
     chartModel?.let { chartsValue ->
-        chartsValue.chart.takeLast(LAST_FIVE_HOURS).map { value ->
+        chartsValue.chart.takeLast(LAST_TWELVE_HOURS).map { value ->
             for (i in value) {
                 dataSet.add(addEntry(value[0], value[1]))
             }
@@ -77,7 +74,7 @@ fun CoinsChart(
                     ).apply {
                     fillDrawable =
                         context.getCompatDrawable(R.drawable.background_transparent_chart)
-                    lineWidth = 1.4f
+                    lineWidth = 1.0f
                 }
             lineChart.apply {
                 setLineDataSet(lineDataSet, 100)
