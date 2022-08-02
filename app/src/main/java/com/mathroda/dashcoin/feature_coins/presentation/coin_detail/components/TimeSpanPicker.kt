f06202162b6ca27d3197ae38234eefdbafb1b870
John_Dominic <johndominicjasmin@gmail.com>
2022-08-02 21:41:59 +0800

    Select time span from TimeSpanPicker

--- app/src/main/java/com/mathroda/dashcoin/feature_coins/presentation/coin_detail/components/TimeSpanPicker.kt
+++ app/src/main/java/com/mathroda/dashcoin/feature_coins/presentation/coin_detail/components/TimeSpanPicker.kt
@@ -1,106 +1,125 @@
 package com.mathroda.dashcoin.feature_coins.presentation.coin_detail.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mathroda.dashcoin.feature_coins.domain.models.ChartTimeSpan
+import com.mathroda.dashcoin.ui.theme.GreenBlue600
 
 
-@Preview
-@Composable
-fun TimeSpanPickerPreview() {
-    TimeSpanPicker(modifier = Modifier.fillMaxWidth())
-}
+
 
 @Composable
 fun TimeSpanPicker(
     modifier: Modifier = Modifier,
-    selectedTimeSpan: ChartTimeSpan = ChartTimeSpan.TimeSpan1Day,
+    selectedTimeSpan: String ,
     onTimeSpanSelected: (ChartTimeSpan) -> Unit = {}
 ) {
-    Row(
+    LazyRow(
         modifier = modifier,
         horizontalArrangement = Arrangement.SpaceAround
     ) {
+        item{
 
-        TimeSpanChip(
-            time = "24H",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpan1Day
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpan1Day)
+            with(ChartTimeSpan.OneHour) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.OneDay) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.OneWeek) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.OneMonth) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.ThreeMonths) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.SixMonths) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.OneYear) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
+
+            with(ChartTimeSpan.All) {
+                TimeSpanChip(
+                    time = value.uppercase(),
+                    isSelected = selectedTimeSpan == value
+                ) {
+                    onTimeSpanSelected(this)
+                }
+            }
         }
 
-        TimeSpanChip(
-            time = "1W",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpan1Week
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpan1Week)
-        }
-
-        TimeSpanChip(
-            time = "1M",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpan1Month
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpan1Month)
-        }
-
-        TimeSpanChip(
-            time = "3M",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpan3Months
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpan3Months)
-        }
-
-        TimeSpanChip(
-            time = "6M",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpan6Months
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpan6Months)
-        }
-
-        TimeSpanChip(
-            time = "1Y",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpan1Year
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpan1Year)
-        }
-
-        TimeSpanChip(
-            time = "ALL",
-            isSelected = selectedTimeSpan == ChartTimeSpan.TimeSpanAll
-        ) {
-            onTimeSpanSelected(ChartTimeSpan.TimeSpanAll)
-        }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TimeSpanChip(
     time: String,
     isSelected: Boolean,
     onTimeSpanSelected: () -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .background(
-                color = if (isSelected) MaterialTheme.colors.onBackground else MaterialTheme.colors.background,
-                shape = RoundedCornerShape(20.dp)
-            )
-            .clickable {
-                onTimeSpanSelected()
-            }
-    ) {
+
+    Chip(
+        onClick = onTimeSpanSelected,
+        modifier = Modifier,
+        shape = RoundedCornerShape(16.dp),
+        colors = ChipDefaults.chipColors(backgroundColor = if (isSelected) GreenBlue600 else MaterialTheme.colors.background)) {
         Text(
             text = time,
-            color = if (isSelected) MaterialTheme.colors.background else MaterialTheme.colors.onBackground,
-            modifier = Modifier.padding(8.dp)
-                )
+            color = if (isSelected) Color.White else MaterialTheme.colors.onBackground,
+        )
     }
 }
+
