236d2541d59efeaf259a91f56913302ef489d2c2
John_Dominic <johndominicjasmin@gmail.com>
2022-08-03 21:02:25 +0800

    Add colors

--- app/src/main/java/com/mathroda/dashcoin/ui/theme/Color.kt
+++ app/src/main/java/com/mathroda/dashcoin/ui/theme/Color.kt
@@ -2,20 +2,27 @@ package com.mathroda.dashcoin.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkGray = Color(0xFF181720)
+val DarkGray = Color(0xFF161520)//background
 val LighterGray = Color(0xFF262532)
 val LightGray = Color(0xFF1E1C25)
-val CustomGreen = Color(0xFF30E0A1)
-val CustomRed= Color(0xFFF6543E)
-val TextWhite = Color(0xFFEEEEEE)
+
 val Gold = Color(0xFFE6C149)
 val Twitter = Color(0xFF00ACEE)
 val Blue100 = Color(0xFF73A9C0)
 
 
-val GREEN600 = Color(0xFF00BDB0)//PRIMARY
+val Red900= Color(0xFFDC1010)
+val Red20 = Color(0xFFFBDBDB)
+
+
+val GreenBlue600 = Color(0xFF00BDB0)//PRIMARY
+
+
+val Green800 = Color(0xFF30E0A1)
+val Green20 = Color(0xFF10DC78)
+
 val Black930 = Color(0xFF292929)
-val Black920 = Color(0xFF252525)// should be use for surfaces
+val Black920 = Color(0xFF252525)//   surfaces
 val Black910 = Color(0xFF313131)
 val Black900 = Color(0xFF303234)
 val Black850 = Color(0xFF5A5A5A)
@@ -25,3 +32,10 @@ val Black450 = Color(0xFF8C8C8C)
 val Black445 = Color(0xFF4D4D4D)
 val Black440 = Color(0xFF979797)
 val Black300 = Color(0xFF919191)
+
+val White800 = Color(0xFFEEEEEE)
+
+
+
+
+
