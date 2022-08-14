3e9c777ff48298378fa14d1ebc3e4befd5221010
John_Dominic <johndominicjasmin@gmail.com>
2022-08-14 22:58:49 +0800

    Add divider on bottom bar

--- app/src/main/java/com/dominic/coin_search/navigation/MainScreen.kt
+++ app/src/main/java/com/dominic/coin_search/navigation/MainScreen.kt
@@ -3,18 +3,22 @@ package com.dominic.coin_search.navigation
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.dominic.coin_search.ui.theme.LighterGray
-import com.dominic.coin_search.ui.theme.White800
+import com.dominic.coin_search.ui.theme.Black800
+import com.dominic.coin_search.ui.theme.Black920
 
 @ExperimentalMaterialApi
 @Composable
@@ -48,48 +52,56 @@ fun BottomBar(
         Screens.CoinsNews
     )
 
-    AnimatedVisibility(
-        visible = state.value,
-        enter = slideInVertically(initialOffsetY = { it }),
-        exit = slideOutVertically(targetOffsetY = { it }),
-    ){
-        BottomNavigation(
-            backgroundColor = LighterGray,
+
+
+        AnimatedVisibility(
+            visible = state.value,
+            enter = slideInVertically(initialOffsetY = { it }),
+            exit = slideOutVertically(targetOffsetY = { it }),
         ) {
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentRoute = navBackStackEntry?.destination?.route
+            Column {
 
-            screens.forEach { screen ->
+            Divider(color = Black800, modifier = Modifier.fillMaxWidth(), thickness = 0.9.dp)
 
-                BottomNavigationItem(
-                    label = {
-                        Text(text = screen.title!!)
-                    },
-                    icon = {
-                        when(screen.icon){
-                            is Int -> {
-                                Icon(painter = painterResource(screen.icon) , contentDescription = null)
+            BottomNavigation(
+                backgroundColor = Black920,
+            ) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                screens.forEach { screen ->
+
+                    BottomNavigationItem(
+                        label = {
+                            Text(text = screen.title!!)
+                        },
+                        icon = {
+                            when (screen.icon) {
+                                is Int -> {
+                                    Icon(
+                                        painter = painterResource(screen.icon),
+                                        contentDescription = null)
+                                }
+                                is ImageVector -> {
+                                    Icon(imageVector = screen.icon, contentDescription = null)
+                                }
                             }
-                            is ImageVector -> {
-                                Icon(imageVector = screen.icon , contentDescription = null)
-                            }
-                        }
-                    },
+                        },
 
-                    selected = currentRoute == screen.route,
+                        selected = currentRoute == screen.route,
 
-                    onClick = {
-                        navController.navigateScreen(screen.route)
-                    },
+                        onClick = {
+                            navController.navigateScreen(screen.route)
+                        },
 
-                    alwaysShowLabel = false,
-                    selectedContentColor = White800,
-                    unselectedContentColor = White800.copy(alpha = ContentAlpha.disabled)
-                )
+                        alwaysShowLabel = false,
+                        selectedContentColor = Color.White,
+                        unselectedContentColor = Color.White.copy(alpha = ContentAlpha.disabled)
+                    )
+                }
             }
         }
     }
-
 
 }
 
