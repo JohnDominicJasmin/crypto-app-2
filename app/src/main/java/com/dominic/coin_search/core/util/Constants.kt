06d29576c2be5468a5b22bfa1ee1bb70eaa8db35
John_Dominic <johndominicjasmin@gmail.com>
2022-08-19 15:59:20 +0800

    Format millis to X Time Ago

--- app/src/main/java/com/dominic/coin_search/core/util/Constants.kt
+++ app/src/main/java/com/dominic/coin_search/core/util/Constants.kt
@@ -9,7 +9,6 @@ object Constants {
     const val COIN_PAPRIKA_BASE_URL = "https://api.coinpaprika.com/"
     const val PARAM_COIN_ID = "coinId" //default value for get coin by id parameter
     const val DATABASE_NAME = "coins_db"
-    const val HANDPICKED_NEWS = "handpicked"
     const val TRENDING_NEWS = "trending"
     const val LATEST_NEWS = "latest"
     const val BULLISH_NEWS = "bullish"
@@ -23,11 +22,14 @@ object Constants {
     val CURRENCY_SYMBOL = stringPreferencesKey("currency_symbol_using")
     val CHART_PERIOD = stringPreferencesKey("chart_period")
 
-    @OptIn(ExperimentalTime::class)
     val UPDATE_INTERVAL = 35.seconds
 
     const val PRICE_ANIMATION_INTERVAL = 800
     const val COINS_LIMIT = 500
     const val GOOGLE_SEARCH_QUERY = "https://www.google.com/search?q="
+    private const val SECOND_MILLIS = 1000
+    const val MINUTE_MILLIS = 60 * SECOND_MILLIS
+    const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+    const val DAY_MILLIS = 24 * HOUR_MILLIS
 
 }
