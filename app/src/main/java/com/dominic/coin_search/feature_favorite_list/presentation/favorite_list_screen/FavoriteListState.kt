4a1b0f0dff15861b5d559944ec63e3b5b3b70fc8
John_Dominic <johndominicjasmin@gmail.com>
2022-08-20 14:15:29 +0800

    Remove undo feature in snackbar

--- app/src/main/java/com/dominic/coin_search/feature_favorite_list/presentation/favorite_list_screen/FavoriteListState.kt
+++ app/src/main/java/com/dominic/coin_search/feature_favorite_list/presentation/favorite_list_screen/FavoriteListState.kt
@@ -1,8 +1,10 @@
 package com.dominic.coin_search.feature_favorite_list.presentation.favorite_list_screen
 
-import com.dominic.coin_search.feature_coins.domain.models.CoinDetailModel
+import com.dominic.coin_search.feature_coins.domain.models.coin.CoinDetailModel
+import com.dominic.coin_search.feature_coins.domain.models.news.NewsModel
 
 data class FavoriteListState(
     val coins: List<CoinDetailModel> = emptyList(),
-    val recentlyDeletedCoin: CoinDetailModel? = null,
+    val news: List<NewsModel> = emptyList(),
+
 )
