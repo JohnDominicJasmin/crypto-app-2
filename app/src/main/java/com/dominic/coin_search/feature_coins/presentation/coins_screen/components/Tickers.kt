4b2b2a2f56f6760b884099a9fb31b7efd7822aa7
John_Dominic <johndominicjasmin@gmail.com>
2022-09-19 21:50:50 +0800

    Change tickers implementation

--- app/src/main/java/com/dominic/coin_search/feature_coins/presentation/coins_screen/components/Tickers.kt
+++ app/src/main/java/com/dominic/coin_search/feature_coins/presentation/coins_screen/components/Tickers.kt
@@ -1,141 +1,145 @@
 package com.dominic.coin_search.feature_coins.presentation.coins_screen.components
 
-import androidx.compose.animation.core.*
-import androidx.compose.material.LocalTextStyle
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dominic.coin_search.ui.theme.Black450
 
 
 @Composable
 fun MarqueeText(
     annotatedText: AnnotatedString,
     modifier: Modifier = Modifier,
-    textModifier: Modifier = Modifier,
-    color: Color = Color.Unspecified,
-    fontSize: TextUnit = TextUnit.Unspecified,
-    fontStyle: FontStyle? = null,
-    fontWeight: FontWeight? = null,
-    fontFamily: FontFamily? = null,
-    letterSpacing: TextUnit = TextUnit.Unspecified,
-    textDecoration: TextDecoration? = null,
-    textAlign: TextAlign? = null,
-    lineHeight: TextUnit = TextUnit.Unspecified,
-    overflow: TextOverflow = TextOverflow.Clip,
-    softWrap: Boolean = true,
-    onTextLayout: (TextLayoutResult) -> Unit = {},
-    style: TextStyle = LocalTextStyle.current,
-    durationMs: Int = 7500,
-    delayMs: Int = 3000,
-    spaceRatio: Float = 0.3f,
 ) {
-    val createText = @Composable { localModifier: Modifier ->
-        Text(
-            annotatedText,
-            textAlign = textAlign,
-            modifier = localModifier,
-            color = color,
-            fontSize = fontSize,
-            fontStyle = fontStyle,
-            fontWeight = fontWeight,
-            fontFamily = fontFamily,
-            letterSpacing = letterSpacing,
-            textDecoration = textDecoration,
-            lineHeight = lineHeight,
-            overflow = overflow,
-            softWrap = softWrap,
-            maxLines = 1,
-            onTextLayout = onTextLayout,
-            style = style,
-        )
+    val scrollState = rememberScrollState()
+    val shouldAnimate by rememberSaveable {
+        mutableStateOf(true)
     }
-    val textLayoutInfoState = remember { mutableStateOf<TextLayoutInfo?>(null) }
-    val textLayoutInfo = textLayoutInfoState.value
-    val transition = rememberInfiniteTransition()
-    val initialValue = 0
-    val offset by if (textLayoutInfo != null) {
-        val duration = durationMs * textLayoutInfo.textWidth / textLayoutInfo.containerWidth
-        transition.animateValue(
-            initialValue = initialValue,
-            targetValue = -textLayoutInfo.textWidth,
-            typeConverter = Int.VectorConverter,
+    LaunchedEffect(key1 = shouldAnimate){
+        scrollState.animateScrollTo(
+            scrollState.maxValue,
             animationSpec = infiniteRepeatable(
-                animation = tween(
-                    durationMillis = duration,
-                    delayMillis = delayMs,
-                    easing = LinearEasing,
-                ),
+                animation = tween(18000, 200, easing = LinearEasing),
                 repeatMode = RepeatMode.Restart
-            ),
-        )
-    } else {
-        remember { mutableStateOf(initialValue) }
-    }
-
-    SubcomposeLayout(
-        modifier = modifier.clipToBounds()
-    ) { constraints ->
-        val infiniteWidthConstraints = constraints.copy(maxWidth = Constraints.Infinity)
-        var mainText = subcompose(MarqueeLayers.MainText) {
-            createText(textModifier)
-        }.first().measure(infiniteWidthConstraints)
-
-        var trailingPlaceableWithOffset: Pair<Placeable, Int>? = null
-        if (mainText.width <= constraints.maxWidth) {
-            mainText = subcompose(MarqueeLayers.TrailingText) {
-                createText(textModifier)
-            }.first().measure(constraints)
-            textLayoutInfoState.value = null
-        } else {
-            val spacing = (constraints.maxWidth * spaceRatio).toInt()
-            textLayoutInfoState.value = TextLayoutInfo(
-                textWidth = mainText.width + spacing, // テキストの長さ + widthの2/3の長さ
-                containerWidth = constraints.maxWidth // 画面幅
             )
+        )
 
-            val trailingTextOffset = mainText.width + spacing + offset
-            val trailingTextSpace = constraints.maxWidth - trailingTextOffset
-            if (trailingTextSpace > 0) {
-                trailingPlaceableWithOffset = subcompose(MarqueeLayers.TrailingText) {
-                    createText(textModifier)
-                }.first().measure(constraints) to trailingTextOffset
-            }
-        }
 
-        layout(
-            width = constraints.maxWidth,
-            height = mainText.height
-        ) {
-            mainText.place(offset, 0)
-            trailingPlaceableWithOffset?.let { (placeable, trailingTextOffset) ->
-                placeable.place(trailingTextOffset, 0)
-            }
-        }
     }
+    Text(
+        text = annotatedText,
+        color = Color.White,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1,
+        fontSize = 14.sp,
+        textAlign = TextAlign.Center,
+        modifier = modifier.horizontalScroll(scrollState, false)
+    )
 }
 
+@Preview
+@Composable
+fun MarqueeTextPreview() {
+    MarqueeText(
+        modifier = Modifier.padding(top = 5.dp, bottom = 8.dp),
+        annotatedText = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Black450)) {
+                append("Number of Cryptos: ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White)) {
+                append("${999999}        |        ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Black450)) {
+                append("Market Cap: ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White)) {
+                append("$${99999}        |        ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Black450)) {
+                append("24h Vol: ")
+            }
 
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White)) {
+                append("$${9999999}        |        ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Black450)) {
+                append("BTC Dominance: ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White)) {
+                append("${99}%        |        ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Black450)) {
+                append("Market Cap All-Time High: ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White)) {
+                append("${999999}        |        ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Black450)) {
+                append("Volume All-Time High(24): ")
+            }
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White)) {
+                append("999")
+            }
 
-private enum class MarqueeLayers { MainText, TrailingText }
-private data class TextLayoutInfo(
-    val textWidth: Int,
-    val containerWidth: Int
-)
+        },
+
+        )
+}
